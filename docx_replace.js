ed3e56d240d327f3b29a7d44d32ad0b95caaf5f0
aliarslanansari <aliarslan1620@gmail.com>
2020-07-18 21:24:32 +0530

    added Doc replace file

--- docx_replace.js
+++ docx_replace.js
@@ -14,3 +14,16 @@ function replaceErrors(key, value) {
     }
     return value;
 }
+function errorHandler(error) {
+    console.log(JSON.stringify({error: error}, replaceErrors));
+
+    if (error.properties && error.properties.errors instanceof Array) {
+        const errorMessages = error.properties.errors.map(function (error) {
+            return error.properties.explanation;
+        }).join("\n");
+        console.log('errorMessages', errorMessages);
+        // errorMessages is a humanly readable message looking like this :
+        // 'The tag beginning with "foobar" is unopened'
+    }
+    throw error;
+}
