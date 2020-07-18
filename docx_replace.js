ab364a8a3b83baa589ac5f88754c0961db492fd5
aliarslanansari <aliarslan1620@gmail.com>
2020-07-18 21:25:17 +0530

    added Doc replace file

--- docx_replace.js
+++ docx_replace.js
@@ -27,3 +27,35 @@ function errorHandler(error) {
     }
     throw error;
 }
+//Load the docx file as a binary
+var content = fs
+    .readFileSync(path.resolve(__dirname, '/home/ali/Desktop/Untitled 1.docx'), 'binary');
+
+var zip = new PizZip(content);
+var doc;
+try {
+    doc = new Docxtemplater(zip);
+} catch(error) {
+    // Catch compilation errors (errors caused by the compilation of the template : misplaced tags)
+    errorHandler(error);
+}
+
+//set the templateVariables
+doc.setData({
+    hello: 'John',
+});
+
+try {
+    // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+    doc.render()
+}
+catch (error) {
+    // Catch rendering errors (errors relating to the rendering of the template : angularParser throws an error)
+    errorHandler(error);
+}
+
+var buf = doc.getZip()
+             .generate({type: 'nodebuffer'});
+
+// buf is a nodejs buffer, you can either write it to a file or do anything else with it.
+fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
